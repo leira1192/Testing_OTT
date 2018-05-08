a68c5f935c7643600da4eba8725884deafb1e7fc
Ariel <leira1192@gmail.com>
2018-05-08 15:58:07 -0600

    update for caph implementation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,95 @@
-var checkTime;
+//inicializar Variables
+video_play();
+console.log('Ya valimos mil madres');
+loadApiContent();
+var actualTime;
+var keys = {};
+var content_api = [];
+var click_item_focus_testing = 0;
+var successCallback = function () {
+	console.log('successCallback its ok');
+};
+var errorCallback = function () {
+	console.log('errorCallback hubo un error');
+};
+window.onload = function () {
+	//Create video object
+	console.log('hemos llegado hasta acá exitosamente');
+	var objElem = document.createElement('object');
+	objElem.type = 'application/avplayer';
+	objElem.style.width = '1920px';
+	objElem.style.height = '1080px';
+	//Insert video object as a div child
+	document.getElementById('video_visible').appendChild(objElem);
+	document.getElementById('video_visible').style.display = 'none';
+	//prepare library avplay
+	webapis.avplay.open('https://www.w3schools.com/html/mov_bbb.mp4');
+	webapis.avplay.setListener(listener);
+	webapis.avplay.setDisplayMethod('PLAYER_DISPLAY_MODE_LETTER_BOX');
+	webapis.avplay.prepare();
+	webapis.avplay.prepareAsync(successCallback, errorCallback);
+	//functions controller buttoms actions
+	registerKeys();
+	bindEvents();
+};
+
+var listener = {
+	onbufferingstart: function (percent) {
+		console.log('Buffering start.' + percent);
+	},
+    
+	onbufferingprogress: function (percent) {
+		console.log('Buffering progress data : ' + percent);
+	},
+
+	onbufferingcomplete: function () {
+		console.log('Buffering complete.');
+	},
+	onstreamcompleted: function () {
+		console.log('Stream Completed');
+		document.getElementById('video_visible').style.display = 'none';
+		webapis.avplay.stop();
+	},
+
+	oncurrentplaytime: function (currentTime) {
+		console.log('Current playtime: ' + currentTime);
+		actualTime = currentTime;
+	},
+
+	onerror: function (eventType) {
+		console.log('event type error : ' + eventType);
+	},
+
+	onevent: function (eventType, eventData) {
+		console.log('event type: ' + eventType + ', data: ' + eventData);
+	},
+
+	onsubtitlechange: function (duration, text, data3, data4) {
+		console.log('subtitleText: ' + text);
+	},
+	ondrmevent: function (drmEvent, drmData) {
+		console.log('DRM callback: ' + drmEvent + ', data: ' + drmData);
+        }
+};
+ 
+
+function registerKeys() {
+	var supportedKeys = tizen.tvinputdevice.getSupportedKeys(), i = 0;
+	for (i = 0; i < supportedKeys.length; i++) {
+		try {
+			tizen.tvinputdevice.registerKey(supportedKeys[i].name);
+		} catch (e) {
+			console.error('failed to register' + supportedKeys[i].name, e);
+		}
+		keys[supportedKeys[i].code] = supportedKeys[i].name;
+		console.log('registro ' + i + ': complete' + keys + ',  keys storage: name:' + keys[supportedKeys[i].code] + ', code: ' + supportedKeys[i].code);
+	}
+	console.log('keys registers');
+	for (var j = 0; j < keys.length; j++) {
+		console.log(keys[j]);
+	}
+
+}
 
 //Initialize function
 var init = function () {
